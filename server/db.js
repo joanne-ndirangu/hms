d34957a5cc3dd74c9560b6a33ae37323098aa1c8
joanne-ndirangu <joannendirangu15@gmail.com>
2026-01-18 13:32:00 +0300

    Updated url

--- server/db.js
+++ server/db.js
@@ -3,6 +3,7 @@ require('dotenv').config({ path: __dirname + '/.env' });
 
 // Create a connection to the database
 const db = mysql.createPool({
+  uri: process.env.DB_URL,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
